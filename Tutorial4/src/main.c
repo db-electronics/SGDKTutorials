feea7b0c3924405e6c2d9648b89cd50c663f1a1b
René Richard <rene@db-elec.com>
2015-02-13 13:30:53 -0500

    Added tut3 and tut4

---
+++ Tutorial4/src/main.c
@@ -0,0 +1,7 @@
+#include <genesis.h>
+
+int main()
+{
+        VDP_drawText("Hello World!", 10, 13);
+        return (0);
+}
---
+++ Tutorial4/src/z80Test.h
@@ -0,0 +1,6 @@
+#ifndef _Z80TEST_H_
+#define _Z80TEST_H_
+
+extern const u8 z80Test[0x9];
+
+#endif // _Z80TEST_H_
