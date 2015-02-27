59b9d572bbd83aa58c3921f31b4ed20277b44b62
René Richard <rene@db-elec.com>
2015-02-27 10:54:24 -0500

    Tone playing on PSG working

--- Tutorial4/src/main.c
+++ Tutorial4/src/main.c
@@ -1,25 +1,32 @@
 #include <genesis.h>
 #include <z80Test.h>
 
-u16 psgTone;
+#define PSGNOTEMIN      45
+#define PSGNOTEMAX      116
+
+u8 psgTone;
 u16 isPlaying;
 
 //prototypes
 void myJoyHandler( u16 joy, u16 changed, u16 state);
-void playTone(u16 tone, u16 stop);
+void playTone(u8 tone, u16 stop);
 
 int main()
 {
+
+    //lowest midi note on PSG is 45, max is 116
+    psgTone = PSGNOTEMIN;
+
     //load Z80 test driver, reset z80 when done
     Z80_init();
-    Z80_upload(0x0000, z80Test, 0x241, 1);
+    Z80_upload(0x0000, z80Test, 0x2000, 1);
 
     //init gamepad
     JOY_init();
     JOY_setEventHandler( &myJoyHandler );
 
     //small greeting
-    VDP_drawText("db Z80 custom driver test", 1, 10);
+    VDP_drawText("db Z80 custom driver test", 1, 1);
 
     //main loop
     while(1)
@@ -39,22 +46,19 @@ int main()
     return 0;
 }
 
-void playTone(u16 tone, u16 stop)
+void playTone(u8 tone, u16 stop)
 {
     Z80_requestBus(1);
 
     if (stop)
     {
         //issue command to stop tone
-        Z80_write(0x0100, 0x00);
+        Z80_write(0x0100, 0x80);
 
     }else
     {
         //write command to play tone
-        Z80_write(0x0100, 0x80);
-        //write tone value, big endian
-        Z80_write(0x0102, (u8)(psgTone>>8));
-        Z80_write(0x0103, (u8)(psgTone));
+        Z80_write(0x0100, psgTone);
     }
 
     Z80_releaseBus();
@@ -67,10 +71,18 @@ void myJoyHandler( u16 joy, u16 changed, u16 state)
         if (state & BUTTON_UP)
         {
             psgTone++;
+            if (psgTone > PSGNOTEMAX)
+            {
+                psgTone = PSGNOTEMIN;
+            }
         }
         else if (state & BUTTON_DOWN)
         {
             psgTone--;
+            if (psgTone < PSGNOTEMIN)
+            {
+                psgTone = PSGNOTEMAX;
+            }
         }
         else if (state & BUTTON_C)
         {
--- Tutorial4/src/z80Test.h
+++ Tutorial4/src/z80Test.h
@@ -1,6 +1,6 @@
 #ifndef _Z80TEST_H_
 #define _Z80TEST_H_
 
-extern const u8 z80Test[0x241];
+extern const u8 z80Test[0x2000];
 
 #endif // _Z80TEST_H_
