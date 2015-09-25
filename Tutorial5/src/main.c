5ec260a47306b0bbd545d2517b6cb45532699b65
René Richard <rene@db-elec.com>
2015-09-25 16:14:16 -0400

    palette fading

--- Tutorial5/src/main.c
+++ Tutorial5/src/main.c
@@ -5,22 +5,34 @@
 
 int main()
 {
+    int i = 0;
+
     Z80_init();
     VDP_setPlanSize(64,64);
     VDP_setScreenHeight240();
     VDP_setScreenWidth320();
     VDP_setScrollingMode(HSCROLL_PLANE,VSCROLL_PLANE);
 
-    VDP_setPalette(PAL1, (const u16 *)palDat);
-    VDP_loadTileData((const u32 *)tileDat, TILE_USERINDEX, 191, 1);
+    //VDP_setPalette(PAL0, (const u16 *)palDat);
+    VDP_loadTileData((const u32 *)tileDat, 0, 191, 1);
 
-    VDP_setTileMapData(VDP_PLAN_A, (const u16 *)mapDat, TILE_USERINDEX, (40*16), 1);
+    for (i=0;i<16;i++)
+    {
+       VDP_setTileMapData(VDP_PLAN_A, (const u16 *)(&mapDat[i*40*2]), i*64, 40, 1);
+    }
 
-    VDP_waitVSync();
+    VDP_fadePalIn(PAL0,(const u16 *)palDat, 30, 0);
+
+    i = 0;
+    while(i<120)
+    {
+        i++;
+        VDP_waitVSync();
+    }
+    VDP_fadePalOut(PAL0, 30, 0);
+
     while(TRUE)
     {
-
-
         VDP_waitVSync();
     }
 }
